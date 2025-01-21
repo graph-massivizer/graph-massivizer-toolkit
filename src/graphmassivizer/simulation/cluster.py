0c8f28c7ccbf9a0dbc259cbf2b0eb90cd9016ec9
Michael Cochez <miselico@users.noreply.github.com>
2025-01-21 13:24:54 +0100

    Cleaning types

--- src/graphmassivizer/simulation/cluster.py
+++ src/graphmassivizer/simulation/cluster.py
@@ -1,15 +1,19 @@
+from .node import Node
+from .network import Network
+
+
 class Cluster:
-    def __init__(self, network) -> None:
-        self.nodes = {}
+    def __init__(self, network: Network) -> None:
+        self.nodes: dict[str, Node] = {}
         self.network = network
         self.network.register_cluster(self)
 
-    def add_node(self, node) -> None:
+    def add_node(self, node: Node) -> None:
         self.nodes[node.node_id] = node
         node.start()
         print(f"Node {node.node_id} added to cluster.")
 
-    def remove_node(self, node_id) -> None:
+    def remove_node(self, node_id: str) -> None:
         if node_id in self.nodes:
             node = self.nodes[node_id]
             node.shutdown()
@@ -20,4 +24,5 @@ class Cluster:
     def monitor_cluster(self) -> None:
         for node in self.nodes.values():
             status = node.report_status()
-            print(f"Node {status['node_id']}: Status {status['status']}, Task Queue Length {status['task_queue_length']}")
+            print(
+                f"Node {status['node_id']}: Status {status['status']}, Task Queue Length {status['task_queue_length']}")
