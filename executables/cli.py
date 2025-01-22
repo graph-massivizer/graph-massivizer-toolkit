4dc560fce79892835ce90f720ff6d6f33f778619
Michael Cochez <miselico@users.noreply.github.com>
2025-01-23 00:57:44 +0100

    Disables some old dependencies, moved the main executable

--- executables/cli.py
+++ executables/cli.py
@@ -1,9 +1,18 @@
+import click
+
 from graphmassivizer.infrastructure.simulation.lifecycle import Simulation
+from graphmassivizer.runtime.task_manager import main as task_manager_main
 
 # https://docs.google.com/drawings/d/1FC5paw_2A3nFBcIW7s99Pk1I_bSUXy_2Uma1LtBmQ_c/edit?usp=sharing
 
 
-def main():
+@click.group()
+def main() -> None:
+    """Graph-Massivizer CLI"""
+
+
+@main.command()
+def simulate_environment():
 
     import logging
 
@@ -18,6 +27,26 @@ def main():
     except Exception:
         simulation.fail()
         raise
+
+
+@main.group()
+def tm():
+    """Commands for the task manager"""
+
+
+@tm.command(name="start")
+def tm_start():
+    task_manager_main.main()
+
+
+@main.group()
+def wf():
+    """Commands for the workflow manager"""
+
+
+@wf.command(name="start")
+def wf_start():
+    task_manager_main.main()
 
 
 if __name__ == '__main__':
