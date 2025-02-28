feac68775bb594229366f57c8f9da3c49e030b3d
Michael Cochez <miselico@users.noreply.github.com>
2025-02-28 10:20:55 +0100

    Completed UC-0 implementations, these still need to be modified to use the graph handle

--- src/graphmassivizer/runtime/task_manager/BGO/networkx_bgos.py
+++ src/graphmassivizer/runtime/task_manager/BGO/networkx_bgos.py
@@ -37,16 +37,25 @@ class BetweennessCentrality(BGO):
         self.out = output_path
 
     def run(self):
-        with open(self.input_path):
-            bfs_graph = pickle.load()
+        with open(self.input_path, "rb") as input:
+            bfs_graph = pickle.load(input)
         betweenness = nx.betweenness_centrality(bfs_graph)
-        with open(self.out) as out:
-            pickle.dump(betweenness, )
+        with open(self.out, "wb") as out:
+            pickle.dump(betweenness, out)
 
 
 class FindMax(BGO):
-
     """Find Max betweenness"""
 
+    def __init__(self, input_path: pathlib.Path, output_path: pathlib.Path) -> None:
+        self.input_path = input_path
+        self.out = output_path
 
-class FindDistance()
+    def run(self):
+        with open(self.input_path, "rb") as input:
+            betweenness = pickle.load(input)
+
+        def f(k_v): return k_v[1]
+        max_betweenness = max(betweenness.items(), key=f)
+        with open(self.out, "wb") as out:
+            pickle.dump(max_betweenness, out)
